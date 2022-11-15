64883be76c802658da77a382d0b081e6c0f3fb1e
masaya.toda <masaya.toda@nextbeat.net>
2022-11-15 14:31:17 +0900

    Todo一覧をD疎通するように修正

--- app/controllers/Todo.scala
+++ app/controllers/Todo.scala
@@ -3,14 +3,16 @@ package controllers
 import javax.inject._
 import java.awt.Desktop.Action
 import java.time.LocalDateTime
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{Success, Failure}
 import play.api._
 import play.api.mvc._
 import play.api.data.Form
 import play.api.data.Forms._
 
 import lib.model.Todo
-import lib.model.Todo._
-import lib.persistence._
+import lib.persistence.default.TodoRepository
 
 import model._
 
@@ -18,14 +20,18 @@ import model._
 class TodoController @Inject()(
   val controllerComponents: ControllerComponents
 ) extends BaseController {
-  def index() = Action { implicit req =>
-    val vv = ViewValueTodo(
-      data = Seq(Todo(Some(Id(1)),1,"タイトル","本文",LocalDateTime.now,LocalDateTime.now))
-    )
-    Ok(views.html.todo.list(vv)(views.html.todo.add(vv)))
+  def index() = Action async { implicit req =>
+    for {
+      todo <- TodoRepository.index()
+    } yield {
+      val vv = ViewValueTodo(
+        data = todo
+      )
+      Ok(views.html.todo.list(vv)(views.html.todo.add(vv)))
+    }
   }
   def show(id:Long) = Action { implicit req =>
-    val testTodo = Todo(Some(Id(id)),1,"タイトル","本文",LocalDateTime.now,LocalDateTime.now)
-    Ok(views.html.todo.show(testTodo))
+    val vv = Todo(Some(Todo.Id(1)),1,"タイトル1","本文1",Todo.Status(0), LocalDateTime.now,LocalDateTime.now)
+    Ok(views.html.todo.show(vv))
   }
 }

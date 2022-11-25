5c07457deaf8ab5ed199bd8a746582337490757f
masaya.toda <masaya.toda@nextbeat.net>
2022-11-25 11:32:31 +0900

    何故かGitHub上でインデントが壊れる問題を調整(暫定対応)

--- app/model/Message.scala
+++ app/model/Message.scala
@@ -9,11 +9,11 @@ case class MessageJson(
 )
 object  MessageJson {
 
-	implicit val reads: Reads[MessageJson]   = Json.reads[MessageJson]
+	implicit val reads:  Reads[MessageJson]  = Json.reads[MessageJson]
   implicit val writes: Writes[MessageJson] = Json.writes[MessageJson]
   def write(message: String): MessageJson = {
     MessageJson(
-      message           = message
+      message = message
     )
   }
 }
